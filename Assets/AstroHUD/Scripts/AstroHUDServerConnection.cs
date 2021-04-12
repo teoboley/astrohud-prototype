493383cc159b867bcaf4ee4b57ab5efb47ead53c
Teo Boley <teo751@gmail.com>
2021-04-12 17:35:57 -0400

    Imported biometrics UI as package

---
+++ Assets/AstroHUD/Scripts/AstroHUDServerConnection.cs
@@ -0,0 +1,387 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+[System.Serializable] public class _UnityEventMessage : UnityEvent<IMessage> { }
+
+public class AstroHUDServerConnection : MonoBehaviour
+{
+    [Header("On message event")]
+    public _UnityEventMessage messageReceived;
+
+
+    // State object for receiving data from remote device.  
+    //public class StateObject
+    //{
+    //    // Client socket.  
+    //    public Socket workSocket = null;
+    //    // Size of receive buffer.  
+    //    public const int BufferSize = 1024;
+    //    // Receive buffer.  
+    //    public byte[] buffer = new byte[BufferSize];
+    //    // Received data string.  
+    //    public StringBuilder sb = new StringBuilder();
+    //}
+
+    // The port number for the remote device.  
+    //private IPAddress ipAddress = IPAddress.Parse("192.168.1.21");
+    //private const int port = 5000;
+    private Uri uri = new Uri("ws://192.168.1.21:5000");
+
+    // ManualResetEvent instances signal completion.  
+    //private static ManualResetEvent connectDone =
+    //    new ManualResetEvent(false);
+    //private static ManualResetEvent sendDone =
+    //    new ManualResetEvent(false);
+    //private static ManualResetEvent receiveDone =
+    //    new ManualResetEvent(false);
+
+    // The response from the remote device.  
+    //private static String response = String.Empty;
+
+    //Socket client;
+
+    public ClientWebSocket clientWebSocket;
+
+    // Start is called before the first frame update
+    async void Start()
+    {
+        clientWebSocket = new ClientWebSocket();
+
+        Debug.Log("[WS]:Attempting connection.");
+        try
+        {
+            await clientWebSocket.ConnectAsync(uri, CancellationToken.None);
+            if (clientWebSocket.State == WebSocketState.Open)
+            {
+                await SendMessage(new HelloMessage());
+                while (true)
+                {
+                    // receive
+                    ArraySegment<byte> bytesReceived = new ArraySegment<byte>(new byte[1024]);
+                    WebSocketReceiveResult result = await clientWebSocket.ReceiveAsync(
+                        bytesReceived,
+                        CancellationToken.None
+                    );
+                    String response = Encoding.UTF8.GetString(bytesReceived.Array, 0, result.Count);
+                    Debug.Log("[WS]Received: " + response);
+                    IMessage responseMsg = JsonConvert.DeserializeObject<IMessage>(response);
+                    messageReceived.Invoke(responseMsg);
+                }
+            }
+            Debug.Log("[WS][connect]:" + "Connected");
+        }
+        catch (WebSocketException e)
+        {
+            Debug.Log("[WS][exception]:" + e.GetType() + " " + e.Message);
+            if (e.InnerException != null)
+            {
+                Debug.Log("[WS][inner exception]:" + e.InnerException.Message);
+            }
+        }
+    }
+
+    async void OnDestroy()
+    {
+        await clientWebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, null, CancellationToken.None);
+    }
+
+    //void Start()
+    //{
+    //    if (messageReceived == null)
+    //    {
+    //        Debug.Log("Creating server message event ref");
+    //        messageReceived = new _UnityEventMessage();
+    //    }
+
+    //    // Connect to a remote device.  
+    //    try
+    //    {
+    //        // Establish the remote endpoint for the socket.  
+    //        IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+
+    //        // Create a TCP/IP socket.  
+    //        client = new Socket(ipAddress.AddressFamily,
+    //            SocketType.Stream, ProtocolType.Tcp);
+
+    //        // Connect to the remote endpoint.  
+    //        client.BeginConnect(remoteEP,
+    //            new AsyncCallback(ConnectCallback), client);
+    //        connectDone.WaitOne();
+
+    //        Debug.Log("Connected to server");
+
+    //        // Send test data to the remote device.  
+    //        SendMessage(new Message<bool>("HELLO", false));
+    //        sendDone.WaitOne();
+
+    //        //// Receive the response from the remote device.  
+    //        Receive(client);
+
+    //        //// Write the response to the console.  
+    //        //Console.WriteLine("Response received : {0}", response);
+
+    //        //// Release the socket.  
+    //        //client.Shutdown(SocketShutdown.Both);
+    //        //client.Close();
+
+    //    }
+    //    catch (Exception e)
+    //    {
+    //        Console.WriteLine(e.ToString());
+    //    }
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private async Task SendMessage<T>(Message<T> message)
+    {
+        ArraySegment<byte> bytesToSend = new ArraySegment<byte>(
+            Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message))
+        );
+        await clientWebSocket.SendAsync(
+            bytesToSend,
+            WebSocketMessageType.Text,
+            true,
+            CancellationToken.None
+        );
+    }
+
+    //private void SendMessage<T>(Message<T> message)
+    //{
+    //    Send(client, JsonUtility.ToJson(message));
+    //}
+
+    //private static void ConnectCallback(IAsyncResult ar)
+    //{
+    //    try
+    //    {
+    //        // Retrieve the socket from the state object.  
+    //        Socket client = (Socket)ar.AsyncState;
+
+    //        // Complete the connection.  
+    //        client.EndConnect(ar);
+
+    //        Console.WriteLine("Socket connected to {0}",
+    //            client.RemoteEndPoint.ToString());
+
+    //        // Signal that the connection has been made.  
+    //        connectDone.Set();
+    //    }
+    //    catch (Exception e)
+    //    {
+    //        Console.WriteLine(e.ToString());
+    //    }
+    //}
+
+    //private void Receive(Socket client)
+    //{
+    //    Debug.Log("Awaiting message from server");
+    //    try
+    //    {
+    //        // Create the state object.  
+    //        StateObject state = new StateObject();
+    //        state.workSocket = client;
+
+    //        // Begin receiving the data from the remote device.  
+    //        client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+    //            new AsyncCallback(ReceiveCallback), state);
+    //    }
+    //    catch (Exception e)
+    //    {
+    //        Console.WriteLine(e.ToString());
+    //    }
+    //}
+
+    //private void ReceiveCallback(IAsyncResult ar)
+    //{
+    //    try
+    //    {
+    //        // Retrieve the state object and the client socket
+    //        // from the asynchronous state object.  
+    //        StateObject state = (StateObject)ar.AsyncState;
+    //        Socket client = state.workSocket;
+
+    //        // Read data from the remote device.  
+    //        int bytesRead = client.EndReceive(ar);
+    //        Debug.Log("Bytes read: " + bytesRead);
+
+    //        if (bytesRead > 0)
+    //        {
+    //            // There might be more data, so store the data received so far.  
+    //            state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+
+    //            // Get the rest of the data.  
+    //            client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+    //                new AsyncCallback(ReceiveCallback), state);
+    //        }
+    //        else
+    //        {
+    //            Debug.Log("No bytes read");
+    //            // All the data has arrived; put it in response.  
+    //            if (state.sb.Length > 1)
+    //            {
+    //                Debug.Log("Complete message from server received");
+    //                response = state.sb.ToString();
+    //                IMessage responseMsg = JsonUtility.FromJson<IMessage>(response);
+    //                messageReceived.Invoke(responseMsg);
+    //            }
+    //            // Signal that all bytes have been received.  
+    //            receiveDone.Set();
+
+    //            // restart receive loop
+    //            Receive(client);
+    //        }
+    //    }
+    //    catch (Exception e)
+    //    {
+    //        Console.WriteLine(e.ToString());
+    //    }
+    //}
+
+    //private void Send(Socket client, String data)
+    //{
+    //    // Convert the string data to byte data using ASCII encoding.  
+    //    byte[] byteData = Encoding.ASCII.GetBytes(data);
+
+    //    // Begin sending the data to the remote device.  
+    //    client.BeginSend(byteData, 0, byteData.Length, 0,
+    //        new AsyncCallback(SendCallback), client);
+    //}
+
+    //private void SendCallback(IAsyncResult ar)
+    //{
+    //    try
+    //    {
+    //        // Retrieve the socket from the state object.  
+    //        Socket client = (Socket)ar.AsyncState;
+
+    //        // Complete sending the data to the remote device.  
+    //        int bytesSent = client.EndSend(ar);
+    //        Console.WriteLine("Sent {0} bytes to server.", bytesSent);
+
+    //        // Signal that all bytes have been sent.  
+    //        sendDone.Set();
+    //    }
+    //    catch (Exception e)
+    //    {
+    //        Console.WriteLine(e.ToString());
+    //    }
+    //}
+}
+
+[Serializable]
+public struct IMessage
+{
+    public String type;
+    public JRaw payload;
+}
+
+[Serializable]
+public abstract class Message<T>
+{
+    public String type;
+    public T payload;
+
+    protected Message(String type, T payload)
+    {
+        this.type = type;
+        this.payload = payload;
+    }
+
+    protected Message(IMessage message)
+    {
+        this.type = message.type;
+        this.payload = PayloadFromJson(message.payload);
+    }
+
+    public abstract T PayloadFromJson(JRaw payload);
+}
+
+// MESSAGE TYPES
+
+public class HelloMessage : Message<bool>
+{
+    public HelloMessage() : base("HELLO", false) { }
+
+    public override bool PayloadFromJson(JRaw payload)
+    {
+        return false;
+    }
+}
+
+public class SystemStateMessage : Message<SystemStateMessagePayload>
+{
+    public SystemStateMessage(IMessage message) : base(message) { }
+
+    public override SystemStateMessagePayload PayloadFromJson(JRaw payload)
+    {
+        return JsonConvert.DeserializeObject<SystemStateMessagePayload>(payload.ToString());
+    }
+
+    public static bool Is(IMessage message)
+    {
+        return message.type == "SYSTEM_STATE";
+    }
+}
+
+[Serializable]
+public struct SystemStateMessagePayload
+{
+    public LifeSupportState lifeSupportState;
+    //public NavigationState navigationState;
+}
+
+[Serializable]
+public struct LifeSupportState {
+    public BodyState bodyState;
+    public SuitState suitState;
+}
+
+[Serializable]
+public struct BodyState {
+    // kCal
+    public int caloriesBurned;
+    // Degrees celsius
+    public float bodyTemperature;
+}
+
+[Serializable]
+public struct SuitState {
+    // Milliampere hours
+    public int currentBattery;
+    // Milliampere hours
+    public int maxBattery;
+    // Milliampere hours per minute
+    public float batteryDrain;
+    // Litres of liquid?
+    public int maxOxygen;
+    // Litres of liquid?
+    public int currentOxygen;
+    // PSI
+    public int tankPressure;
+    // Litres per minute
+    public float currentOxygenConsumption;
+    // Percentage
+    public float humidity;
+    // Cycles per second
+    public float radioactivity;
+    // BPM
+    public int heartRate;
+    // PSI
+    public int suitPressure;
+}
---
+++ Assets/AstroHUD/Scripts/SetSpatialAnchorsScript.cs
@@ -0,0 +1,350 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Microsoft.Azure.SpatialAnchors.Unity.Examples
+{
+    public class SetSpatialAnchorsScript : SpatialAnchorsUtil
+    {
+        private string currentAnchorId = "";
+
+        /// <summary>
+        /// Start is called on the frame when a script is enabled just before any
+        /// of the Update methods are called the first time.
+        /// </summary>
+        public async override void Start()
+        {
+            Debug.Log(">>Set Spatial Anchors Script Start");
+
+            base.Start();
+
+            if (!SanityCheckAccessConfiguration())
+            {
+                return;
+            }
+            // feedbackBox.text = stateParams[currentAppState].StepMessage;
+
+            Debug.Log("Set Spatial Anchors script started");
+
+            // create session
+            Debug.Log("Creating Spatial Anchors session");
+            if (CloudManager.Session == null)
+            {
+                await CloudManager.CreateSessionAsync();
+            }
+            currentAnchorId = "";
+            currentCloudAnchor = null;
+            Debug.Log("Spatial Anchors session created");
+
+            // configure session
+            Debug.Log("Configuring Spatial Anchors session");
+            ConfigureSession();
+            Debug.Log("Spatial Anchors session configured");
+
+            // start session
+            Debug.Log("Starting Spatial Anchors session");
+            await CloudManager.StartSessionAsync();
+            Debug.Log("Spatial Anchors session started");
+
+            // add message listeners from server
+            serverConnection.messageReceived.AddListener(OnServerMessageReceived);
+        }
+
+        public override void OnDestroy()
+        {
+            base.OnDestroy();
+
+            // stop session
+            CloudManager.StopSession();
+            currentWatcher = null;
+
+            // complete cleanup
+            currentCloudAnchor = null;
+            CleanupSpawnedObjects();
+        }
+
+        /*protected override void OnCloudAnchorLocated(AnchorLocatedEventArgs args)
+        {
+            base.OnCloudAnchorLocated(args);
+
+            if (args.Status == LocateAnchorStatus.Located)
+            {
+                currentCloudAnchor = args.Anchor;
+
+                UnityDispatcher.InvokeOnAppThread(() =>
+                {
+                    Pose anchorPose = Pose.identity;
+
+#if UNITY_ANDROID || UNITY_IOS
+                    anchorPose = currentCloudAnchor.G
+                    etPose();
+#endif
+                    // HoloLens: The position will be set based on the unityARUserAnchor that was located.
+                    SpawnOrMoveCurrentAnchoredObject(anchorPose.position, anchorPose.rotation);
+                    // currentAppState = AppState.DemoStepDeleteFoundAnchor;
+                });
+            }
+        }*/
+
+        /// <summary>
+        /// Update is called every frame, if the MonoBehaviour is enabled.
+        /// </summary>
+        public override void Update()
+        {
+            base.Update();
+
+            if (spawnedObjectMat != null)
+            {
+                float rat = 0.1f;
+                float createProgress = 0f;
+                if (CloudManager.SessionStatus != null)
+                {
+                    createProgress = CloudManager.SessionStatus.RecommendedForCreateProgress;
+                }
+                rat += (Mathf.Min(createProgress, 1) * 0.9f);
+                spawnedObjectMat.color = Color.blue * rat;
+            }
+        }
+
+        public async Task addAnchorAtGazePoint()
+        {
+            RaycastHit hit;
+            if (base.TryGazeHitTest(out hit)) {
+                // add local anchor
+                Quaternion rotation = Quaternion.AngleAxis(0, Vector3.up);
+                GameObject newGameObject = base.SpawnNewAnchoredObject(hit.point, rotation);
+                newGameObject.GetComponent<MeshRenderer>().material.color = Color.blue;
+
+                // upload local anchor
+                await base.SaveObjectAnchorToCloudAsync(newGameObject);
+            }
+        }
+
+        protected override async Task OnSaveCloudAnchorSuccessfulAsync(GameObject anchorObject)
+        {
+            await base.OnSaveCloudAnchorSuccessfulAsync(anchorObject);
+
+            anchorObject.GetComponent<MeshRenderer>().material.color = Color.yellow;
+
+            Debug.Log("Anchor created, yay!");
+
+            currentAnchorId = currentCloudAnchor.Identifier;
+
+            // Sanity check that the object is still where we expect
+            Pose anchorPose = Pose.identity;
+
+#if UNITY_ANDROID || UNITY_IOS
+            anchorPose = currentCloudAnchor.GetPose();
+#endif
+            // HoloLens: The position will be set based on the unityARUserAnchor that was located.
+
+            // FIXME: SpawnOrMoveAnchoredObject(currentCloudAnchor, anchorPose.position, anchorPose.rotation);
+
+            // currentAppState = AppState.DemoStepStopSession;
+        }
+
+        protected override void OnSaveCloudAnchorFailed(GameObject anchorObject, Exception exception)
+        {
+            base.OnSaveCloudAnchorFailed(anchorObject, exception);
+
+            anchorObject.GetComponent<MeshRenderer>().material.color = Color.red;
+
+            currentAnchorId = string.Empty;
+        }
+
+        private void OnServerMessageReceived(IMessage message)
+        {
+            //Output message to the console
+            Debug.Log("Server Message Received - " + message.type);
+        }
+
+        //internal enum AppState
+        //{
+        //    DemoStepCreateSession = 0,
+        //    DemoStepConfigSession,
+        //    DemoStepStartSession,
+        //    DemoStepCreateLocalAnchor,
+        //    DemoStepSaveCloudAnchor,
+        //    DemoStepSavingCloudAnchor,
+        //    DemoStepStopSession,
+        //    DemoStepDestroySession,
+        //    DemoStepCreateSessionForQuery,
+        //    DemoStepStartSessionForQuery,
+        //    DemoStepLookForAnchor,
+        //    DemoStepLookingForAnchor,
+        //    DemoStepDeleteFoundAnchor,
+        //    DemoStepStopSessionForQuery,
+        //    DemoStepComplete,
+        //    DemoStepBusy
+        //}
+
+        //private readonly Dictionary<AppState, DemoStepParams> stateParams = new Dictionary<AppState, DemoStepParams>
+        //{
+        //    { AppState.DemoStepCreateSession,new DemoStepParams() { StepMessage = "Next: Create Azure Spatial Anchors Session", StepColor = Color.clear }},
+        //    { AppState.DemoStepConfigSession,new DemoStepParams() { StepMessage = "Next: Configure Azure Spatial Anchors Session", StepColor = Color.clear }},
+        //    { AppState.DemoStepStartSession,new DemoStepParams() { StepMessage = "Next: Start Azure Spatial Anchors Session", StepColor = Color.clear }},
+        //    { AppState.DemoStepCreateLocalAnchor,new DemoStepParams() { StepMessage = "Tap a surface to add the Local Anchor.", StepColor = Color.blue }},
+        //    { AppState.DemoStepSaveCloudAnchor,new DemoStepParams() { StepMessage = "Next: Save Local Anchor to cloud", StepColor = Color.yellow }},
+        //    { AppState.DemoStepSavingCloudAnchor,new DemoStepParams() { StepMessage = "Saving local Anchor to cloud...", StepColor = Color.yellow }},
+        //    { AppState.DemoStepStopSession,new DemoStepParams() { StepMessage = "Next: Stop Azure Spatial Anchors Session", StepColor = Color.green }},
+        //    { AppState.DemoStepCreateSessionForQuery,new DemoStepParams() { StepMessage = "Next: Create Azure Spatial Anchors Session for query", StepColor = Color.clear }},
+        //    { AppState.DemoStepStartSessionForQuery,new DemoStepParams() { StepMessage = "Next: Start Azure Spatial Anchors Session for query", StepColor = Color.clear }},
+        //    { AppState.DemoStepLookForAnchor,new DemoStepParams() { StepMessage = "Next: Look for Anchor", StepColor = Color.clear }},
+        //    { AppState.DemoStepLookingForAnchor,new DemoStepParams() { StepMessage = "Looking for Anchor...", StepColor = Color.clear }},
+        //    { AppState.DemoStepDeleteFoundAnchor,new DemoStepParams() { StepMessage = "Next: Delete Anchor", StepColor = Color.yellow }},
+        //    { AppState.DemoStepStopSessionForQuery,new DemoStepParams() { StepMessage = "Next: Stop Azure Spatial Anchors Session for query", StepColor = Color.grey }},
+        //    { AppState.DemoStepComplete,new DemoStepParams() { StepMessage = "Next: Restart demo", StepColor = Color.clear }},
+        //    { AppState.DemoStepBusy,new DemoStepParams() { StepMessage = "Processing...", StepColor = Color.clear }}
+        //};
+
+        //private AppState _currentAppState = AppState.DemoStepCreateSession;
+
+        //AppState currentAppState
+        //{
+        //    get
+        //    {
+        //        return _currentAppState;
+        //    }
+        //    set
+        //    {
+        //        if (_currentAppState != value)
+        //        {
+        //            Debug.LogFormat("State from {0} to {1}", _currentAppState, value);
+        //            _currentAppState = value;
+        //            if (spawnedObjectMat != null)
+        //            {
+        //                spawnedObjectMat.color = stateParams[_currentAppState].StepColor;
+        //            }
+
+        //            if (!isErrorActive)
+        //            {
+        //                feedbackBox.text = stateParams[_currentAppState].StepMessage;
+        //            }
+        //        }
+        //    }
+        //}
+
+        //protected override bool IsPlacingObject()
+        //{
+        //    return currentAppState == AppState.DemoStepCreateLocalAnchor;
+        //}
+
+        //protected override Color GetStepColor()
+        //{
+        //    return stateParams[currentAppState].StepColor;
+        //}
+
+        //public async override Task AdvanceDemoAsync()
+        //{
+        //    switch (currentAppState)
+        //    {
+        //        case AppState.DemoStepCreateSession:
+        //            currentAppState = AppState.DemoStepBusy;
+        //            if (CloudManager.Session == null)
+        //            {
+        //                await CloudManager.CreateSessionAsync();
+        //            }
+        //            currentAnchorId = "";
+        //            currentCloudAnchor = null;
+        //            currentAppState = AppState.DemoStepConfigSession;
+        //            break;
+        //        case AppState.DemoStepConfigSession:
+        //            currentAppState = AppState.DemoStepBusy;
+        //            ConfigureSession();
+        //            currentAppState = AppState.DemoStepStartSession;
+        //            break;
+        //        case AppState.DemoStepStartSession:
+        //            currentAppState = AppState.DemoStepBusy;
+        //            await CloudManager.StartSessionAsync();
+        //            currentAppState = AppState.DemoStepCreateLocalAnchor;
+        //            break;
+        //        case AppState.DemoStepCreateLocalAnchor:
+        //            currentAppState = AppState.DemoStepBusy;
+        //            if (spawnedObject != null)
+        //            {
+        //                currentAppState = AppState.DemoStepSaveCloudAnchor;
+        //            }
+        //            else
+        //            {
+        //                currentAppState = AppState.DemoStepCreateLocalAnchor;
+        //            }
+        //            break;
+        //        case AppState.DemoStepSaveCloudAnchor:
+        //            currentAppState = AppState.DemoStepSavingCloudAnchor;
+        //            await SaveCurrentObjectAnchorToCloudAsync();
+        //            break;
+        //        case AppState.DemoStepStopSession:
+        //            currentAppState = AppState.DemoStepBusy;
+        //            CloudManager.StopSession();
+        //            CleanupSpawnedObjects();
+        //            await CloudManager.ResetSessionAsync();
+        //            currentAppState = AppState.DemoStepCreateSessionForQuery;
+        //            break;
+        //        case AppState.DemoStepCreateSessionForQuery:
+        //            ConfigureSession();
+        //            currentAppState = AppState.DemoStepStartSessionForQuery;
+        //            break;
+        //        case AppState.DemoStepStartSessionForQuery:
+        //            currentAppState = AppState.DemoStepBusy;
+        //            await CloudManager.StartSessionAsync();
+        //            currentAppState = AppState.DemoStepLookForAnchor;
+        //            break;
+        //        case AppState.DemoStepLookForAnchor:
+        //            currentAppState = AppState.DemoStepLookingForAnchor;
+        //            if (currentWatcher != null)
+        //            {
+        //                currentWatcher.Stop();
+        //                currentWatcher = null;
+        //            }
+        //            currentWatcher = CreateWatcher();
+        //            if (currentWatcher == null)
+        //            {
+        //                Debug.Log("Either cloudmanager or session is null, should not be here!");
+        //                feedbackBox.text = "YIKES - couldn't create watcher!";
+        //                currentAppState = AppState.DemoStepLookForAnchor;
+        //            }
+        //            break;
+        //        case AppState.DemoStepLookingForAnchor:
+        //            break;
+        //        case AppState.DemoStepDeleteFoundAnchor:
+        //            currentAppState = AppState.DemoStepBusy;
+        //            await CloudManager.DeleteAnchorAsync(currentCloudAnchor);
+        //            CleanupSpawnedObjects();
+        //            currentAppState = AppState.DemoStepStopSessionForQuery;
+        //            break;
+        //        case AppState.DemoStepStopSessionForQuery:
+        //            currentAppState = AppState.DemoStepBusy;
+        //            CloudManager.StopSession();
+        //            currentWatcher = null;
+        //            currentAppState = AppState.DemoStepComplete;
+        //            break;
+        //        case AppState.DemoStepComplete:
+        //            currentAppState = AppState.DemoStepBusy;
+        //            currentCloudAnchor = null;
+        //            CleanupSpawnedObjects();
+        //            currentAppState = AppState.DemoStepCreateSession;
+        //            break;
+        //        case AppState.DemoStepBusy:
+        //            break;
+        //        default:
+        //            Debug.Log("Shouldn't get here for app state " + currentAppState.ToString());
+        //            break;
+        //    }
+        //}
+
+        private void ConfigureSession()
+        {
+            List<string> anchorsToFind = new List<string>();
+            // FIXME: re-enable for query
+            //if (currentAppState == AppState.DemoStepCreateSessionForQuery)
+            //{
+            //    anchorsToFind.Add(currentAnchorId);
+            //}
+
+            SetAnchorIdsToLocate(anchorsToFind);
+        }
+    }
+}
