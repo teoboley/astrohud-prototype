6e8de4da36cf99ad819bc98fa5f06b5174ddeee2
Joseph Annis <annis.jo@husky.neu.edu>
2021-04-18 12:22:04 -0400

    Lots of waypoint stuff.

---
+++ Assets/WaypointController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class WaypointController : MonoBehaviour
+{
+    public WaypointType type;
+    public string waypointLabel;
+
+    public TextMeshPro NameLabel;
+    public TextMeshPro DistanceLabel;
+    public TextMeshPro TravelTimeLabel;
+
+    public GameObject HeadObject;
+    public GameObject TailObject;
+
+    public Material PrimaryMaterial;
+    public Material SecondaryMaterial;
+    public Material PingMaterial;
+
+
+    public enum WaypointType
+    {
+        Primary,
+        Secondary,
+        Ping
+    }
+
+    void Start()
+    {
+        var material = GetMaterial();
+        HeadObject.GetComponent<MeshRenderer>().sharedMaterial = material;
+        TailObject.GetComponent<MeshRenderer>().sharedMaterial = material;
+        NameLabel.text = waypointLabel;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private Material GetMaterial()
+    {
+        switch(type)
+        {
+            case WaypointType.Primary:
+                return PrimaryMaterial;
+            case WaypointType.Secondary:
+                return SecondaryMaterial;
+            default:
+                return PingMaterial;
+        }
+    }
+}
---
+++ Assets/WaypointManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointManager : MonoBehaviour
+{
+    public GameObject WaypointPrefab;
+    public GameObject NewWaypointRoot;
+    public LineRenderer LineRenderer;
+
+
+    private List<GameObject> Waypoints = new List<GameObject>();
+    private bool shouldUpdate = false;
+    private LineRenderer lineRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    //    var line = LineRenderer.gameObject;
+    //  line.transform.position = new Vector3(line.transform.position.x, -0.6f, line.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shouldUpdate)
+        {
+            Debug.Log("Updating path.");
+            shouldUpdate = false;
+            RenderWaypointLines();
+        }
+    }
+
+    public void CreateWaypoint()
+    {
+        Debug.Log("Creating waypoint.");
+        var waypoint = Instantiate(WaypointPrefab);
+        waypoint.transform.position = NewWaypointRoot.transform.position;
+
+        Waypoints.Add(waypoint);
+        //shouldUpdate = true;
+    }
+
+    public void RenderWaypointLines()
+    {
+        List<Vector3> waypointPositions = new List<Vector3>();
+
+
+        foreach (GameObject waypoint in Waypoints)
+        {
+            waypoint.transform.position = new Vector3(waypoint.transform.position.x, -0.6f, transform.position.z);
+            waypointPositions.Add(waypoint.transform.position);
+        }
+
+        LineRenderer.positionCount = waypointPositions.Count;
+        LineRenderer.SetPositions(waypointPositions.ToArray());
+    }
+}
