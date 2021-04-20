f24b05a14c530c25679480be816e50a5676b0b43
Joseph Annis <annis.jo@husky.neu.edu>
2021-04-20 14:26:24 -0400

    Add astronaut card.

---
+++ Assets/AstronautCardController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+
+public class AstronautCardController : MonoBehaviour
+{
+    [Header("Render Targets")]
+    public TextMeshProUGUI AstronautName;
+    public TextMeshProUGUI TaskDescription;
+    public Image HeartRate;
+    public Image WaterLevel;
+    public Image BatteryLevel;
+    public Image OxygenLevel;
+    public Image TemperatureLevel;
+
+    [Header("Content")]
+    public string astronautName;
+    
+    public string currentTask;
+    public Status currentTaskStatus;
+
+    public Status heartRateStatus;
+    public Status waterLevelStatus;
+    public Status batteryLevelStatus;
+    public Status oxygenStatus;
+    public Status temperatureStatus;
+
+    [Header("Styling")]
+    public Color positiveStatusColor;
+    public Color warningStatusColor;
+    public Color dangerStatusColor;
+    
+    public enum Status
+    {
+        Positive,
+        Warning,
+        Danger
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HeartRate.color = GetColor(heartRateStatus);
+        WaterLevel.color = GetColor(waterLevelStatus);
+        BatteryLevel.color = GetColor(batteryLevelStatus);
+        OxygenLevel.color = GetColor(oxygenStatus);
+        TemperatureLevel.color = GetColor(temperatureStatus);
+
+        TaskDescription.text = currentTask;
+        AstronautName.text = astronautName;
+    }
+
+    private Color GetColor(Status status)
+    {
+        switch (status)
+        {
+            case Status.Positive:
+                return positiveStatusColor;
+            case Status.Warning:
+                return warningStatusColor;
+            default:
+                return dangerStatusColor;
+        }
+    }
+}
