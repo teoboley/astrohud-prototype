174701980f8707d479eb7115e4e33b3b40791d32
Teo Boley <teo751@gmail.com>
2021-04-21 15:52:30 -0400

    Added indicator and list controllers

---
+++ Assets/AstroHUD/Scripts/Comms/CommsIndicatorController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+
+public class CommsIndicatorController : MonoBehaviour
+{
+    public TextMeshProUGUI AstronautNameLabel;
+    public GameObject ActiveIndicator;
+
+    private string astronautName = "Astronaut";
+    private bool active = false;
+    private Color color = Color.blue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateView();
+    }
+
+    public void SetAstronautName(string name)
+    {
+        astronautName = name;
+        UpdateView();
+    }
+
+    public void SetActive(bool active)
+    {
+        this.active = active;
+        UpdateView();
+    }
+
+    public void SetColor(Color color)
+    {
+        this.color = color;
+        UpdateView();
+    }
+
+    public void SetAllProperties(string name, bool active, Color color)
+    {
+        astronautName = name;
+        this.active = active;
+        this.color = color;
+        UpdateView();
+    }
+
+    public void UpdateView()
+    {
+        AstronautNameLabel.text = astronautName;
+        ActiveIndicator.SetActive(active);
+        ActiveIndicator.GetComponent<Image>().color = this.color;
+        if (active)
+        {
+            AstronautNameLabel.color = this.color;
+        } else
+        {
+            AstronautNameLabel.color = Color.white;
+        }
+    }
+
+    public void ToggleActive()
+    {
+        SetActive(!active);
+    }
+}
---
+++ Assets/AstroHUD/Scripts/Comms/CommsIndicatorListController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class CommsIndicatorListController : MonoBehaviour
+{
+    public GameObject CommsIndicatorPrefab;
+    public GameObject IndicatorContainer;
+
+    private List<AstronautIndicatorData> astronautIndicatorItems;
+    private List<GameObject> indicatorGameObjects = new List<GameObject>();
+
+    public struct AstronautIndicatorData
+    {
+        public string name;
+        public bool active;
+        public Color color;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SetIndicatorItems(List<AstronautIndicatorData> astronautIndicatorItems)
+    {
+        this.astronautIndicatorItems = astronautIndicatorItems;
+        UpdateView();
+    }
+
+    public void UpdateView()
+    {
+        // clear old indicators
+        for (int i = 0; i < indicatorGameObjects.Count; i++)
+        {
+            Destroy(indicatorGameObjects[i]);
+        }
+        this.indicatorGameObjects.Clear();
+
+        // add new indicators
+        for (int i = 0; i < astronautIndicatorItems.Count; i++)
+        {
+            var indicatorData = astronautIndicatorItems[i];
+
+            var indicatorGameObject = Instantiate(CommsIndicatorPrefab, IndicatorContainer.transform);
+            indicatorGameObject.GetComponentInChildren<CommsIndicatorController>().SetAllProperties(indicatorData.name, indicatorData.active, indicatorData.color);
+            this.indicatorGameObjects.Add(indicatorGameObject);
+        }
+    }
+}
